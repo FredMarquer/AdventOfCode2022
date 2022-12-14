ba18b217196df09e82bbf5219b28038461f32345
Fred <fred.35400@live.fr>
2022-12-14 14:01:59 +0100

    Improves days include dependencies

--- src/Days.h
+++ src/Days.h
@@ -1,44 +1,8 @@
 #pragma once
 
 #include "Day.h"
-#include "Day01.h"
-#include "Day02.h"
-#include "Day03.h"
-#include "Day04.h"
-#include "Day05.h"
-#include "Day06.h"
-#include "Day07.h"
-#include "Day08.h"
-#include "Day09.h"
-#include "Day10.h"
-#include "Day11.h"
-#include "Day12.h"
-#include "Log.h"
 
 const int DayCount = 12;
 
-std::unique_ptr<Day> createDay(int dayNumber)
-{
-	Day* dayPtr = nullptr;
-	switch (dayNumber)
-	{
-	case 1: dayPtr = new Day01(); break;
-	case 2: dayPtr = new Day02(); break;
-	case 3: dayPtr = new Day03(); break;
-	case 4: dayPtr = new Day04(); break;
-	case 5: dayPtr = new Day05(); break;
-	case 6: dayPtr = new Day06(); break;
-	case 7: dayPtr = new Day07(); break;
-	case 8: dayPtr = new Day08(); break;
-	case 9: dayPtr = new Day09(); break;
-	case 10: dayPtr = new Day10(); break;
-	case 11: dayPtr = new Day11(); break;
-	case 12: dayPtr = new Day12(); break;
-	default:
-		error("invalid day number: {}", dayNumber);
-		break;
-	}
-
-	return std::unique_ptr<Day>(dayPtr);
-}
+std::unique_ptr<Day> createDay(int dayNumber);
 
