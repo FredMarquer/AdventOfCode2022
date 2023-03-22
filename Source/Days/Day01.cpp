eb2c65e8347409589b77bd669e8be09d15cb7780
Fred <fred.35400@live.fr>
2023-03-22 22:27:57 +0100

    Improves Day 1

--- Source/Days/Day01.cpp
+++ Source/Days/Day01.cpp
@@ -6,52 +6,26 @@
 
 void Day01::parseFile(std::ifstream& file)
 {
-    int calories = 0;
-    std::string line;
+    std::string line{};
+    caloriesPerElves.push_back(0);
     while (std::getline(file, line)) {
-        if (line.empty()) {
-            caloriesPerElves.push_back(calories);
-            calories = 0;
-        }
+        if (line.empty())
+            caloriesPerElves.push_back(0);
         else
-            calories += std::stoi(line);
+            caloriesPerElves.back() += std::stoi(line);
     }
 }
 
 Result Day01::runPart1() const
 {
-    // Find the max calories value
-    int maxCalories = 0;
-    for (int calories : caloriesPerElves) {
-        if (calories > maxCalories)
-            maxCalories = calories;
-    }
-
-    return maxCalories;
+    return std::ranges::max(caloriesPerElves);
 }
 
 Result Day01::runPart2() const
 {
-    // Find the top 3 max calories values
-    std::vector<int> top3(3);
-    for (int calories : caloriesPerElves) {
-        for (size_t i = 0; i < 3; ++i) {
-            if (calories > top3[i]) {
-                top3.insert(top3.begin() + i, calories);
-                top3.pop_back();
-                break;
-            }
-        }
-    }
-
-    assert(top3.size() == 3);
-
-    // Sum the top 3
-    int sum = 0;
-    for (int result : top3)
-        sum += result;
-
-    return sum;
+    std::vector<int> vec = caloriesPerElves;
+    std::nth_element(vec.begin(), vec.begin() + 3, vec.end(), std::greater{});
+    return std::accumulate(vec.begin(), vec.begin() + 3, 0);
 }
 
 Result Day01::getExpectedResultPart1() const
